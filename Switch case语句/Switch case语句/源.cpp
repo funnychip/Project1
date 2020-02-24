d31daf7c7c84c2150bd2e60ca23b53941f490021
funnychip <memory4446@aliyun.com>
2020-02-24 17:52:37 +0800

    I want a "Switch"! Signed-off-by: funnychip <memory4446@aliyun.com>

---
+++ Switch case语句/Switch case语句/源.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void)
+{
+	int a = 4;
+	switch (a)
+	{
+	case 1:
+		printf("it's 1\n");
+		break;
+	case 2:
+		printf("it's 2\n");
+		break;
+	case 3:
+		printf("it's 3\n");
+		break;
+	case 4:
+		printf("it's 4\n");
+		break;
+	case 5:
+		printf("it's 5\n");
+		break;
+	default:
+		printf("it's 0\n");
+		break;
+	}
+}
